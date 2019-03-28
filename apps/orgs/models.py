cd2a75189a7cf76bfc9c070caf890d488097219d
vueyyy_glb <858078219@qq.com>
2019-03-29 00:36:29 +0800

    2019.3.29

--- apps/orgs/models.py
+++ apps/orgs/models.py
@@ -1,3 +1,58 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+#城市表
+class CityInfo(models.Model):
+    name=models.CharField(max_length=20,verbose_name='城市名称')
+    add_time=models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name='城市信息'
+        verbose_name_plural=verbose_name
+
+#机构表
+class OrgInfo(models.Model):
+    image=models.ImageField(max_length=200,verbose_name='机构封面',upload_to='org/')
+    name=models.CharField(max_length=20,verbose_name='机构名称')
+    course_num=models.IntegerField(default=0,verbose_name='课程数')
+    study_num=models.IntegerField(default=0,verbose_name='学习人数')
+    address=models.CharField(max_length=200,verbose_name='机构地址')
+    desc=models.CharField(max_length=200,verbose_name='机构简介')
+    detail=models.TextField(verbose_name='机构详情')
+    love_num=models.IntegerField(default=0,verbose_name='收藏数')
+    click_num=models.IntegerField(default=0,verbose_name='访问量')
+    cetegory=models.CharField(choices=(('pxjg','培训机构'),('gx','高校'),('gr','个人')),max_length=20,verbose_name='机构类别')
+    #一个城市可以有多个机构，一个机构只属于一个城市，也是一对多
+    cityInfo=models.ForeignKey(CityInfo,verbose_name='所在城市',on_delete=models.CASCADE)
+    add_time=models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name='机构信息'
+        verbose_name_plural=verbose_name
+
+#老师表
+class TeacherInfo(models.Model):
+    image = models.ImageField(max_length=200, verbose_name='讲师头像', upload_to='teacher/')
+    name=models.CharField(max_length=20,verbose_name='讲师姓名')
+    work_year=models.IntegerField(default=3,verbose_name='工作年限')
+    work_position=models.CharField(max_length=20,verbose_name='工作职位')
+    work_style=models.CharField(max_length=20,verbose_name='教学特点')
+    #一对多，一个老师只属于一个机构，一个机构内有多个讲师
+    work_company=models.ForeignKey(OrgInfo,verbose_name='所属机构')
+    age=models.IntegerField(default=30,verbose_name='讲师年龄')
+    gender=models.CharField(choices=(('boy','男'),('girl','女')),max_length=20,verbose_name='讲师性别',default='boy')
+    love_num = models.IntegerField(default=0, verbose_name='收藏数')
+    click_num = models.IntegerField(default=0, verbose_name='访问量')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name='讲师信息'
+        verbose_name_plural=verbose_name
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,44 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime
 # Create your models here.
+
+class UserProfile(AbstractUser):
+    image=models.ImageField(upload_to='user/',verbose_name='用户头像',max_length=200,null=True,blank=True)
+    nick_name=models.CharField(max_length=20,verbose_name='用户昵称',null=True,blank=True)
+    birthday=models.DateTimeField(verbose_name='用户生日',null=True,blank=True)
+    gender=models.CharField(verbose_name='性别',choices=(('girl','女'),('boy','男')),max_length=20,default='girl')
+    address=models.CharField(verbose_name='用户地址',max_length=200,null=True,blank=True)
+    phone=models.CharField(verbose_name='用户手机',max_length=11,null=True,blank=True)
+    add_time=models.DateTimeField(default=datetime.now,verbose_name='添加时间')
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name='用户信息'
+        verbose_name_plural=verbose_name
+
+
+class BannerInfo(models.Model):
+    image=models.ImageField(upload_to='banner/',verbose_name='轮播图片',max_length=200)
+    url=models.URLField(default='http://www.atguigu.com',max_length=200,verbose_name='图片链接')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    def __str__(self):
+        return str(self.image)
+
+    class Meta:
+        verbose_name = '轮播图信息'
+        verbose_name_plural = verbose_name
+
+class EmailVerifyCode(models.Model):
+    code=models.CharField(max_length=20,verbose_name='邮箱验证码')
+    email=models.EmailField(max_length=200,verbose_name='验证码邮箱')
+    send_type=models.IntegerField(choices=((1,'register'),(2,'forget'),(3,'change')),verbose_name='验证码类型')
+    def __str__(self):
+        return self.code
+
+    class Meta:
+        verbose_name = '验证码信息'
+        verbose_name_plural = verbose_name
